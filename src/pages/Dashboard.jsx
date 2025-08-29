696b896bed4d03b29e6ec531deb63677410023ab
Kailash Tanwar <kailashrajut0575@gmail.com>
2025-08-29 23:37:06 +0530

    remove sidebar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import Header from '../components/layout/Header';
-import Sidebar from '../components/layout/Sidebar';
+import React from "react";
+import Header from "../components/layout/Header";
+import Sidebar from "../components/layout/Sidebar";
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <Header />
-      <div className="dashboard-content">
+      {/* <div className="dashboard-content">
         <Sidebar />
-       
-      </div>
+      </div> */}
     </div>
   );
 };
 
-export default Dashboard; 
+export default Dashboard;
